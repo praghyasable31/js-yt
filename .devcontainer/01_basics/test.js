a73d992759bd2aab0c9cfa258d29b7e0756ccc76
Praghya Sable <pragyasable314@gmail.com>
2026-02-10 20:27:09 +0000

    01_basics done

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,25 @@
-console.log("pragya");
+const accountId = 144553 //const - value ko change nhi kiya ja skta
+let accountEmail = "pragya31@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState; //undefined value
+//agr js k andr variable declare krke chhod dete h aur 
+// uski value kuch assign nhi krte h toh vo undefined mante h.
+
+//account_Id = 2 //not allowed
+
+accountEmail = "pragya@123gmail.com"
+accountPassword = "0987654321"
+accountCity = "Banglore"
+
+
+// console.log(account_Id);
+// console.log("accountEmail");
+console.table([accountEmail,accountPassword,accountId,accountCity,accountState]);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
