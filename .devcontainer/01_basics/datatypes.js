a33ffffb0a3a9d17c9ce9da146c03aa00c02ec0b
Praghya Sable <pragyasable314@gmail.com>
2026-02-10 20:55:18 +0000

    ..

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict" //treat all JS code as newer version
+
+alert("3+3") //we are using node.js , not browser.
+
+console.log(3+3); console.log("pragya"); //it is wrong 
+//code readability should be high
+
+console.log("pragya sable");
+
+//documentation - MDN , TC-39
+
+
+
+/*
+datatypes :-
+1.number = 2 to power 53
+2.bigint = number bht zyada bda h toh bigint use krte h
+3.string - " "
+4.boolean = true/false
+5.null = standalone value //khali h //empty value
+6.undefined = jb variable declare krdiya ho but value assign nhi ki ho.
+7.symbol = to find uniqueness.
+*/
+
+
+/*
+object :-
+
+*/
+console.log(typeof "pragya"); //o/p = string
+console.log(typeof age); //o/p number
+console.log(typeof null); //o/p object
+console.log(typeof undefined); //o/p undefined
