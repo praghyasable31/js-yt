d3f88cee0114d1cd973830711c871c92c8fae573
Praghya Sable <pragyasable314@gmail.com>
2026-02-11 14:18:01 +0000

    ..

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+ let score = "33abc";
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+ let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// num = 33abc ;op = NaN = not a number
+//num = null ; 0p=0
+//num = undefined ; op= Nan
+//num = true-op = 1 ; false-op = 0
+
+
+let isLoggedIn = "pragyaa" 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//boolean mai 1 converts into true 
+//and 0 converts into false
+
+//1 => true, 0 => false
+//"" => false
+//"pragya" => true
+
+let someNumer = 33
+
+let StringNumber = String(someNumer)
+console.log(StringNumber);
+console.log(typeof StringNumber);
